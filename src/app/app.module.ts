3fcb60cf3577a3ec8bc9938ef6971c1549de29ea
Ahmedgomaaabdelrahman <a.gomaaabdelrahman@gmail.com>
2017-09-19 11:29:38 +0200

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
+import { TermsPage } from './../pages/terms/terms';
+import { SignupPage } from './../pages/signup/signup';
+import { SettingsPage } from './../pages/settings/settings';
+import { SearchPage } from './../pages/search/search';
+import { RatingPage } from './../pages/rating/rating';
+import { PaymentPage } from './../pages/payment/payment';
+import { OrdermapPage } from './../pages/ordermap/ordermap';
+import { OrderdetailsPage } from './../pages/orderdetails/orderdetails';
+import { LoginPage } from './../pages/login/login';
+import { InvoicePage } from './../pages/invoice/invoice';
+import { ForgetpassPage } from './../pages/forgetpass/forgetpass';
+import { FilterPage } from './../pages/filter/filter';
+import { CartPage } from './../pages/cart/cart';
+import { AddlocationPage } from './../pages/addlocation/addlocation';
+import { AboutPage } from './../pages/about/about';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {} from '../pages/';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    AddlocationPage,
+    CartPage,
+    FilterPage,
+    ForgetpassPage,
+    InvoicePage,
+    LoginPage,
+    OrderdetailsPage,
+    OrdermapPage,
+    PaymentPage,
+    RatingPage,
+    SearchPage,
+    SettingsPage,
+    SignupPage,
+    TermsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +49,22 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    AddlocationPage,
+    CartPage,
+    FilterPage,
+    ForgetpassPage,
+    InvoicePage,
+    LoginPage,
+    OrderdetailsPage,
+    OrdermapPage,
+    PaymentPage,
+    RatingPage,
+    SearchPage,
+    SettingsPage,
+    SignupPage,
+    TermsPage
   ],
   providers: [
     StatusBar,
